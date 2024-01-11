0d5dd78a34520279cda5270c8be31341c726e640
WALTERXO <91873172+WALTERXO@users.noreply.github.com>
2024-01-11 19:18:42 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+api_id = 25807666
+api_hash = '96046f216e730c639efcbf33d4967748'
+bot_token = '6933022445:AAEoq6IdTlGfb2fmorWaGdKFP1jfEH_miUQ'
+my_id = '340842516'
+
+technical_channel_id = -1001394809744  # ID технического канала
+
+new_link = "http://t.me/newchenbo"
+replase_link = "@newchenbo"
+
+proxy_url = 'http://91.218.51.212:9703'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "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"  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
