f72e115c6adaa4b0d2fad5e368861c2095843abd
WALTERXO <91873172+WALTERXO@users.noreply.github.com>
2024-01-11 19:19:56 +0300

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = 25807666
-api_hash = '96046f216e730c639efcbf33d4967748'
-bot_token = '6933022445:AAEoq6IdTlGfb2fmorWaGdKFP1jfEH_miUQ'
-my_id = '340842516'
+api_id = 
+api_hash = ''
+bot_token = ''
+my_id = ''
 
-technical_channel_id = -1001394809744  # ID технического канала
+technical_channel_id =   # ID технического канала
 
-new_link = "http://t.me/newchenbo"
-replase_link = "@newchenbo"
+new_link = "http://t.me/"
+replase_link = "@"
 
-proxy_url = 'http://91.218.51.212:9703'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
-openai_api_key = "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"  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
