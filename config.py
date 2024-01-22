254f5cf2006bccfc48d97f84d48c6f2d5e518867
WALTERXO <91873172+WALTERXO@users.noreply.github.com>
2024-01-22 18:42:05 +0300

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ my_id =
 technical_channel_id =   # ID технического канала
 
 new_link = "http://t.me/"
-replase_link = "@"
+new_username = "@"
 
 proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
 openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
